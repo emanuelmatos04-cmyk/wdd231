04c2b8b09037b7b339d8c02c30ba7b720a9cc4ea
emanuelmatos04 <ematosfrancisco@byupathway.du>
2026-08-03 00:27:54 -0400

    W5H

--- chamber/scripts/nav.js
+++ chamber/scripts/nav.js
@@ -3,5 +3,7 @@ const menuButton = document.getElementById("menuButton");
 const menuItems = document.getElementById("menuItems");
 
 menuButton.addEventListener("click", () => {
-  menuItems.classList.toggle("open");
+  const expanded = menuButton.getAttribute("aria-expanded") === "true";
+  menuButton.setAttribute("aria-expanded", !expanded);
+  menuItems.classList.toggle("show");
 });
